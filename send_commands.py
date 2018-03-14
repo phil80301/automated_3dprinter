1a5903dcee74a07a8091ed467bb28ffa4bfaf54c
Philipp Wu <phil80301@berkeley.edu>
2018-03-13 18:13:20 -0700

    sending commands

--- send_commands.py
+++ send_commands.py
@@ -1,7 +1,11 @@
 from printer_driver import Printer
+def removal_pos(p):
+    p.write("G90")
+    p.write("G0 X100 Y100 Z100 F6000")
 
 printer = Printer("/dev/ttyACM0", 9600, verbose=True)
-printer.write("G91")
-printer.write("G1 X30 F4800")
+# printer.write("G91")
 # printer.write("G1 X0 F3600")
 # printer.write("M32 top.gcode")
+removal_pos(printer)
+
