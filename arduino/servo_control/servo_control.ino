e55f6bf910a543ee84b125c9c24a79b0b15901ca
Philipp <phil80301@berkeley.edu>
2018-04-18 14:52:21 -0700

    servo testing

--- arduino/servo_control/servo_control.ino
+++ arduino/servo_control/servo_control.ino
@@ -3,30 +3,100 @@
 Servo left_servo;
 Servo right_servo;
 
-#define LEFT_SERVO_PIN 9
-#define RIGHT_SERVO_PIN 10
+#define LEFT_SERVO_PIN 6
+#define RIGHT_SERVO_PIN 7
 
 #define LEFT_SERVO_DEFAULT_US 1500
 #define RIGHT_SERVO_DEFAULT_US 1500
+#define LIMIT_SWITCH 12
 
+
+int switch_val;
 void setup() {
   Serial.begin(115200);
-  left_servo.attach(LEFT_SERVO_PIN);
-  right_servo.attach(RIGHT_SERVO_PIN);
+  // Setting Up Servos
+  left_servo.attach(5 );
+  //right_servo.attach(RIGHT_SERVO_PIN);
   left_servo.writeMicroseconds(LEFT_SERVO_DEFAULT_US);
-  right_servo.writeMicroseconds(RIGHT_SERVO_DEFAULT_US);
+  //right_servo.writeMicroseconds(RIGHT_SERVO_DEFAULT_US);
 
+  // Setting up LimitSwitch
+  pinMode(LIMIT_SWITCH, INPUT);
+  switch_val = 0;
   Serial.println("Ready");
 }
 
+int mode = -1;
+
+void open90(){
+  right_servo.write(0);
+  left_servo.write(0);
+}
+
+
+void close90(){
+  //right_servo.write(90);
+  left_servo.write(180);
+}
+
+
+void flexup(){
+  right_servo.write(0);
+  left_servo.write(0);
+}
+
+
+
+void flexdown(){
+  right_servo.write(0);
+  left_servo.write(0);
+}
+
+
+void flat(){
+  right_servo.write(0);
+  left_servo.write(0);
+}
+int count = 0;
 void loop() {
-  switch(Serial.read()) {
-    case 'a':
-      left_servo.writeMicroseconds(Serial.parseInt());
-      break;
-    case 'b':
-      right_servo.writeMicroseconds(Serial.parseInt());
-      break;
-  }
+    count = count + 1;
+    if(digitalRead(LIMIT_SWITCH) == HIGH){
+      switch_val = 0;
+    } else {
+      switch_val = 1;
+    }
+
+    if (count%1000 == 1) {
+      //Serial.println(switch_val);
+    }
+
+    if (Serial.available() > 0) {
+      int read_value = Serial.read();
+      mode = read_value - 48;
+      switch(mode) {
+        case 0:
+          open90();
+          Serial.println("done");
+          break;
+        case 1:
+          close90();
+          Serial.println("done");
+          break;
+        case 2:
+          flexup();
+          Serial.println("done");
+          break;
+        case 3:
+          flexdown();
+          Serial.println("done");
+          break;
+        case 4:
+          flat();
+          Serial.println("done");
+          break;
+      }
+      delay(15);  
+    }
+
 }
 
--- arduino/stepper_control/stepper_control.ino
+++ arduino/stepper_control/stepper_control.ino
@@ -17,36 +17,67 @@ void setup()
 {  
   Serial.begin(115200);
   stepper.setMaxSpeed(1000000);
-  stepper.setSpeed(-1000000);	
+  stepper.setSpeed(0);	
   Serial.println("Ready");
   
 }
 
 void switch_zero(){ stepper.setSpeed(0); }
 
-void switch_up(){ stepper.setSpeed(-100000); }
+void switch_up(){ stepper.setSpeed(-10000); }
 
-void switch_down(){ stepper.setSpeed(100000); }
+void switch_down(){ stepper.setSpeed(10000); }
 
+
+void slow_up(){ stepper.setSpeed(-500); }
+
+void slow_down(){ stepper.setSpeed(500); }
+
+void lift_plate(){ 
+   int start_time = millis();
+    while (start_time + 2000 > millis()) {
+      Serial.println(millis());
+      stepper.setSpeed(-10000);
+      stepper.runSpeed();
+    }  
+    stepper.setSpeed(0);
+    stepper.runSpeed();
+    
+}
 
 
 void loop()
 {  
-
+  
     if (Serial.available() > 0) {
       int read_value = Serial.read();
-      mode = read_value;
+      mode = read_value-48;
       switch(mode) {
-        case '0:
+        case 0:
           switch_zero();
+          Serial.println(Serial.read());
           Serial.println("done");
           break;
         case 1:
           switch_up();
+          Serial.println(Serial.read());
           Serial.println("done");
           break;
         case 2:
-          switch_up();
+          switch_down();
+          Serial.println("done");
+          break;
+        case 3:
+          slow_up();
+          Serial.println(Serial.read());
+          Serial.println("done");
+          break;
+        case 4:
+          slow_down();
+          Serial.println("done");
+          break;
+        case 5:
+          lift_plate();
           Serial.println("done");
           break;
       }
