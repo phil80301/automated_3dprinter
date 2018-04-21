4dba508a4373405bbda95a5d60d58af23dbe62a0
allanzhao <allanzhao@users.noreply.github.com>
2018-04-21 16:50:25 -0700

    Added eject sequence to servo_control

--- arduino/servo_control/servo_control.ino
+++ arduino/servo_control/servo_control.ino
@@ -6,15 +6,38 @@ Servo right_servo;
 #define LEFT_SERVO_PIN 9
 #define RIGHT_SERVO_PIN 10
 
-#define LEFT_SERVO_DEFAULT_US 1500
-#define RIGHT_SERVO_DEFAULT_US 1500
+#define LEFT_SERVO_OPEN_US 400
+#define RIGHT_SERVO_OPEN_US 300
+
+#define LEFT_SERVO_CLAMP_US 1300
+#define RIGHT_SERVO_CLAMP_US 1200
+
+#define LEFT_SERVO_FLEX_US 900
+#define RIGHT_SERVO_FLEX_US 1700
+
+void eject() {
+  left_servo.writeMicroseconds(LEFT_SERVO_OPEN_US);
+  right_servo.writeMicroseconds(RIGHT_SERVO_OPEN_US);
+  delay(2000);
+  left_servo.writeMicroseconds(LEFT_SERVO_CLAMP_US);
+  right_servo.writeMicroseconds(RIGHT_SERVO_CLAMP_US);
+  delay(5000);
+  left_servo.writeMicroseconds(LEFT_SERVO_FLEX_US);
+  right_servo.writeMicroseconds(RIGHT_SERVO_FLEX_US);
+  delay(2000);
+  left_servo.writeMicroseconds(LEFT_SERVO_CLAMP_US);
+  right_servo.writeMicroseconds(RIGHT_SERVO_CLAMP_US);
+  delay(2000);
+  left_servo.writeMicroseconds(LEFT_SERVO_OPEN_US);
+  right_servo.writeMicroseconds(RIGHT_SERVO_OPEN_US);
+}
 
 void setup() {
   Serial.begin(115200);
   left_servo.attach(LEFT_SERVO_PIN);
   right_servo.attach(RIGHT_SERVO_PIN);
-  left_servo.writeMicroseconds(LEFT_SERVO_DEFAULT_US);
-  right_servo.writeMicroseconds(RIGHT_SERVO_DEFAULT_US);
+  left_servo.writeMicroseconds(LEFT_SERVO_OPEN_US);
+  right_servo.writeMicroseconds(RIGHT_SERVO_OPEN_US);
 
   Serial.println("Ready");
 }
@@ -26,6 +49,9 @@ void loop() {
       break;
     case 'b':
       right_servo.writeMicroseconds(Serial.parseInt());
+      break;
+    case 'e':
+      eject();
       break;
   }
 }
